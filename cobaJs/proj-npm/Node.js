5e0775d7c4ce031af2cca904b39290b843fdb34d
tafifa <tafifa09@gmail.com>
2023-01-04 11:04:46 +0700

    upload

---
+++ cobaJs/proj-npm/Node.js
@@ -0,0 +1,9 @@
+//    USING moment MODULE
+// const moment = require('moment')
+// const date = moment().format("dddd, MMMM Do YYYY")
+// console.log(date)
+
+//    USING lodash MODULE
+// const lodash = require("lodash")
+// const myOddEvenArray = lodash.partition([1,2,3,4,5,6], (n) => n%2);
+// console.log(myOddEvenArray)
