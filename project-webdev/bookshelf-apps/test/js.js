d669823cc5e040804812019b11fe1bb0b277a4cc
tafifa <tafifa09@gmail.com>
2023-06-11 18:54:35 +0700

    delete project-webdev folder

--- project-webdev/bookshelf-apps/test/js.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("submit").addEventListener(click(), function()
-{
-    const nama = document.getElementById("name").value;
-    const umur = document.getElementById("age").value;
-
-    sessionStorage.setItem("name", nama);
-    sessionStorage.setItem("age", umur);
-});
